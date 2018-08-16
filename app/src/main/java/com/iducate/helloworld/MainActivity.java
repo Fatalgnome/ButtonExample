ae99270d90d1cd490f8cfc608044f8113ea6989c
makn <makn@eaaa.dk>
2018-08-16 13:25:27 +0200

    updated gradle version

--- app/src/main/java/com/iducate/helloworld/MainActivity.java
+++ app/src/main/java/com/iducate/helloworld/MainActivity.java
@@ -5,7 +5,9 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -15,6 +17,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.btnHello);
+      /*  button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(),"Hello class",Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });*/
         System.out.println("martin");
         Log.d("MainActivity","Startup finished");
 
@@ -25,6 +35,7 @@ public class MainActivity extends Activity {
         dialog = new Dialog(this);
         
         dialog.setTitle("greetings"); //hardcoded - should be in strings.xml!
+
 
         TextView textView = new TextView(this);
         //textView.setText("Hello World"); //Hardcoded - not best.
